0c805489b9538668d933ea07e8181857581382ca
tahabellotef <tahabellotef@gmail.com>
2023-10-20 18:46:42 +0100

    yes

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,13 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
+use App\Form\FormAuthorType;
+use App\Repository\AuthorRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorController extends AbstractController
 {
-    
+    #[Route('/author', name: 'app_author')]
     public function index(): Response
     {
         return $this->render('author/index.html.twig', [
@@ -16,53 +22,60 @@ class AuthorController extends AbstractController
         ]);
     }
 
-    
+    #[Route('/showAuthor/{name}',name:'app_showAuthor')]
     public function showAuthor ( $name ){
         return $this->render('author/show.html.twig',[
             'n'=>$name
         ]);
     }
 
-    public function list() 
-    {
-        $authors = array(
-            array('id' => 1, 'picture' => '/images/Victor_Hugo.jpg','username' => ' Victor
-            Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100),
-            array('id' => 2, 'picture' => '/images/william_shakespeare.jpg','username' => '
-            William Shakespeare', 'email' => ' william.shakespeare@gmail.com', 'nb_books' =>
-            200 ),
-            array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => ' Taha
-            Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300),);
-            
-            return $this->render('author/list.html.twig', ['authors' => $authors]);
+    #[Route('/author/get/all',name:'app_get_all_author')]
+    public function getAll(AuthorRepository $repo){
+        $authors = $repo->findAll();
+        return $this->render('author/listauthors.html.twig',[
+            'a'=>$authors
+        ]);
+    }
+
+    //#[Route('/author/add',name:'app_add_author')]
+    //public function add(ManagerRegistry $manager){
+
+      //  $author = new Author();
+      //  $author->setUsername('author 1');
+      //  $author->setEmail('author1@esprit.tn');
+      //  $manager->getManager()->persist($author);
+      //  $manager->getManager()->flush();
+      //  return $this->redirectToRoute('app_get_all_author');
+
+    //}
+    #[Route('/author/delete/{id}',name:'app_delete_author')]
+    public function delete($id,ManagerRegistry $manager,AuthorRepository $repo){
+        $author = $repo->find($id);
+        $manager->getManager()->remove($author);
+        $manager->getManager()->flush();
+        return $this->redirectToRoute('app_get_all_author');
+
+    }
+    #[Route('/author/update/{id}', name: 'app_author_update')]
+    public function updateAuthor($id,ManagerRegistry $manager,AuthorRepository $repo){
+        $author = $repo->find($id);
+        $author->setUsername('author updated');
+        $manager->getManager()->flush();
+        return $this->redirectToRoute('app_author');
+    }
+    #[Route('/author/add', name: 'app_author_add')]
+    public function addAuthor(Request $req ,ManagerRegistry $manager){
+        $author = new Author();
+        $form = $this->createForm(FormAuthorType::class,$author);
+        $form->handleRequest($req);
+        if ($form->isSubmitted()){
+            $author->setNbBooks(0);
+                 $manager->getManager()->persist($author);
+                 $manager->getManager()->flush();
+        return $this->redirectToRoute('app_author');
         }
-        public function authorDetails ($id){
-            $authors = array(
-                array('id' => 1, 'picture' => '/images/Victor_Hugo.jpg','username' => ' Victor
-                Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100),
-                array('id' => 2, 'picture' => '/images/william_shakespeare.jpg','username' => '
-                William Shakespeare', 'email' => ' william.shakespeare@gmail.com', 'nb_books' =>
-                200 ),
-                array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => ' Taha
-                Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300),);
-    
-            $author=NULL;
-            foreach($authors as $a)
-            {
-                if($a['id'] == $id)
-                {
-                    $author =$a;
-                    
-                    break;
-                }
-            }
-    
-            return $this->render('author/showAuthor.html.twig',['author' => $author]);
-        }
-    
+        return $this->render('author/add.html.twig',['a'=>$form->createView()]);
 
-    
-
-
-   
+        
+    }
 }
--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -8,10 +8,30 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TeacherController extends AbstractController
 {
-    public function index(): Response{
-        return new Response('Bonr');
+    #[Route('/teacher', name: 'app_teacher')]
+    public function index(): Response
+    {
+        return new Response("Bonjour");
     }
-    public function showTeacher($name){
-        return new Response('Bon'.$name);
+
+    #[Route('/showteacher/{name}/{id}',name:'app_showteacher')]
+    public function showTeacher($name,$id){
+        return new Response("Bonjour ". $name.' '.$id);
+    }
+
+    #[Route('/show',name:'app_show')]
+    public function show(){
+        $title = "Teacher";
+        $teachers = array (
+            array('id'=>1,'name'=>'test','salaire'=>1000),
+            array('id'=>2,'name'=>'teacher2','salaire'=>2000),
+            array('id'=>3,'name'=>'test','salaire'=>3000)
+        );
+        return $this->render('teacher/show.html.twig',['t'=>$title,'tt'=>$teachers]);
+    }
+
+    #[Route('/details/{id}',name:'app_details')]
+    public function details($id){
+        return $this->render('teacher/details.html.twig',['id'=>$id]);
     }
 }
